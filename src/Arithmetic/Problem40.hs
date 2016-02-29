2dbfd98b6ab146677018fc5aea3dc2fa3ba9c544
krzysiekz <krzysiekcrpgfan@gmail.com>
2016-02-29 13:00:50 +0100

    problem 40

---
+++ src/Arithmetic/Problem40.hs
@@ -0,0 +1,25 @@
+module Arithmetic.Problem40 where
+
+-- (**) Goldbach's conjecture.
+--
+-- Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers.
+-- Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case.
+-- It has been numerically confirmed up to very large numbers (much larger than we can go with our Prolog system).
+-- Write a predicate to find the two prime numbers that sum up to a given even integer.
+--
+-- Example:
+--
+-- * (goldbach 28)
+-- (5 23)
+-- Example in Haskell:
+--
+-- *goldbach 28
+-- (5, 23)
+
+import Arithmetic.Problem39
+
+goldbach :: Int -> (Int, Int)
+goldbach x
+    | x <= 2 || odd x = error "Wrong input!!!"
+    | otherwise = head [(i, j) | i <- primes, j <- primes, i+j==x ]
+    where primes = primesR 1 x
