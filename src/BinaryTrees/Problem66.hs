472e7e5660fca231e82feda52771edcbb556195e
krzysiek <krzysiekcrpgfan@gmail.com>
2016-04-24 15:16:41 +0200

    problem 66

--- src/BinaryTrees/Problem66.hs
+++ src/BinaryTrees/Problem66.hs
@@ -24,30 +24,51 @@ import BinaryTrees.Problem55
 import qualified  BinaryTrees.Problem65 as S
 import qualified Data.List as List
 
-layout :: Tree a -> [(a,(Int, Int))]
-layout = layout' 0 0
+layout :: Tree a -> Tree (a, (Int, Int))
+layout t = t'
+  where (l, t', r) = layoutAux x1 1 t
+        x1 = maximum l + 1
 
-layout' :: Int -> Int -> Tree a -> [(a,(Int, Int))]
-layout' _ _ Empty = []
-layout' n h (Branch val Empty Empty) = [(val, (1+n, 1+h))]
-layout' n h (Branch val left right) =[(val, (diffMiddle , h+1))] ++ leftResult ++ rightResult
-    where leftResult = layout' n (h+1) left
-          x = if not (null leftResult) then fst $ snd $ head leftResult else n
-          rightResult = layout' diffRight (h + 1) right
-          maxR = maxRight left
-          minL = minLeft right
-          diffMiddle = x + if maxR == 0 || minL == 0 then 1 else maxR + minL
-          diffRight = if minL == 0 then diffMiddle else x + (maxR + minL)*2 - minL -1
+        layoutAux :: Int -> Int -> Tree a -> ([Int], Tree (a, (Int, Int)), [Int])
+        layoutAux x y Empty = ([], Empty, [])
+        layoutAux x y (Branch a l r) = (ll', Branch (a, (x,y)) l' r', rr')
+          where (ll, l', lr) = layoutAux (x-sep) (y+1) l
+                (rl, r', rr) = layoutAux (x+sep) (y+1) r
+                sep = maximum (0:zipWith (+) lr rl) `div` 2 + 1
+                ll' = 0 : overlay (map (+sep) ll) (map (subtract sep) rl)
+                rr' = 0 : overlay (map (+sep) rr) (map (subtract sep) lr)
 
-maxX :: [(a, (Int, Int))] -> (a, (Int, Int))
-maxX = List.maximumBy (\ (_, (x,_)) (_, (x2,_)) -> compare x x2)
+-- overlay xs ys = xs padded out to at least the length of ys
+-- using any extra elements of ys
+overlay :: [a] -> [a] -> [a]
+overlay [] ys = ys
+overlay xs [] = xs
+overlay (x:xs) (y:ys) = x : overlay xs ys
 
-minLeft :: Tree a -> Int
-minLeft Empty = 0
-minLeft (Branch _ Empty _) = 0
-minLeft (Branch _ left _) = 1 + minLeft left
+-- layout :: Tree a -> [(a,(Int, Int))]
+-- layout = layout' 0 0
 
-maxRight :: Tree a -> Int
-maxRight Empty = 0
-maxRight (Branch _ _ Empty) = 0
-maxRight (Branch _ _ right) = 1 + maxRight right
+-- layout' :: Int -> Int -> Tree a -> [(a,(Int, Int))]
+-- layout' _ _ Empty = []
+-- layout' n h (Branch val Empty Empty) = [(val, (1+n, 1+h))]
+-- layout' n h (Branch val left right) =[(val, (diffMiddle , h+1))] ++ leftResult ++ rightResult
+--     where leftResult = layout' n (h+1) left
+--           x = if not (null leftResult) then fst $ snd $ head leftResult else n
+--           rightResult = layout' diffRight (h + 1) right
+--           maxR = maxRight left
+--           minL = minLeft right
+--           diffMiddle = x + if maxR == 0 || minL == 0 then 1 else maxR + minL
+--           diffRight = if minL == 0 then diffMiddle else x + (maxR + minL)*2 - minL -1
+--
+-- maxX :: [(a, (Int, Int))] -> (a, (Int, Int))
+-- maxX = List.maximumBy (\ (_, (x,_)) (_, (x2,_)) -> compare x x2)
+--
+-- minLeft :: Tree a -> Int
+-- minLeft Empty = 0
+-- minLeft (Branch _ Empty _) = 0
+-- minLeft (Branch _ left _) = 1 + minLeft left
+--
+-- maxRight :: Tree a -> Int
+-- maxRight Empty = 0
+-- maxRight (Branch _ _ Empty) = 0
+-- maxRight (Branch _ _ right) = 1 + maxRight right
